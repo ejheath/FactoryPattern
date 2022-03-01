a43c8967b75cf14ebdcd5404a82127e8eee963de
Elijah Heath <ejheath8888@gmail.com>
2022-03-01 11:10:25 -0500

    working with interface to exemplify factory patterns

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfWheels;
+            bool input = false;
+            do
+            {
+                Console.WriteLine("Enter wheel count for either car or motorcycle: ")
+
+                input = int.TryParse(Console.ReadLine(), out numOfWheels);
+
+            } while (!input);
+
+            var vehicle = VehicleFactory.GetVehicle(numOfWheels);
+            vehicle.Drive();
+        }
+
+    }
+}
